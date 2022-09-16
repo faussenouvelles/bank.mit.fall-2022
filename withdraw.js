92cedb7c6f54a2d9d785c2630a32173894ff63dd
eletheia_sweet <102085293+faussenouvelles@users.noreply.github.com>
2022-09-16 19:26:26 -0400

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -1,5 +1,14 @@
 function Withdraw(){
+  const ctx = React.useContext(UserContext);
+
+  const updateBalance = () => {
+    ctx.user[0].balance -= 10;
+    console.log(ctx.user[0].balance)
+  }
   return (
     <h1>Withdraw</h1>
-  )
+    {JSON.stringify(ctx)}
+
+    <button onclick={updateBalance}> Withdraw </button>
+  );
 }
